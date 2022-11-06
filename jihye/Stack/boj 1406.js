af55c7b88239a9891beb3b17d6db7d7875a5aa04
jiiihyee <jiiihyeee98@gmail.com>
2022-11-07 00:29:58 +0900

    푸는중

---
+++ jihye/Stack/boj 1406.js
@@ -0,0 +1,14 @@
+let input = require('fs').readFileSync(__dirname+'/input.txt').toString().split('\n');
+var n = parseInt(input[0].split(' ')); //명령 개수 
+//input[1]~input[14]까지의 명령이 있음
+
+
+function solution(n){
+
+}
+
+
+console.log(solution(n))
+
+
+
