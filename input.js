e1adc110583e3e0d5e7ed7fc7030fd2a3ec1d14f
gajeong <rlarwkjd127@gmail.com>
2022-11-06 23:20:42 +0900

    wait

--- input.js
+++ input.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+const fs = require("fs");
 const stdin = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
     : `5 5
     10 25 7 8 13
     68 24 -78 63 32
@@ -9,9 +9,19 @@ const stdin = (
     -16 -22 -57 -33 99
     7 -76 -11 77 15
 `
-).split('\n');
+).split("\n");
 const arr = stdin.map((el) => el.trim());
 
-const solution = (input) => {};
+const solution = (input) => {
+  console.log("gg");
+};
 
 solution(arr);
+
+function solution(A) {
+  let answer = 0;
+  A.forEach((num) => {
+    if (num >= 0 && num < 10) answer = Math.max(answer, num);
+  });
+  return answer;
+}
