a507be837faa24cdb51d25b3b78a788329b1d08d
gajeong <rlarwkjd127@gmail.com>
2022-11-07 01:27:53 +0900

    점점 미궁속으로..

--- gajeong/BackTraking/boj10819.js
+++ gajeong/BackTraking/boj10819.js
@@ -13,35 +13,31 @@ const arr = stdin[1].trim().split(" ").map(Number);
 const solution = (N, arr) => {
   let visited = Array(N).fill(false);
   const result = [];
+  let stack = [];
 
-  for (let i = 0; i < N - 1; i++) {
-    let stack = [arr[i]];
+  for (let i = 0; i < N; i++) {
+    //첫번째 값 넣어줌
+    stack.push(arr[i]);
     visited[i] = true;
-    let sum = backtracking(1, stack, visited, result);
-    visited = Array(N).fill(false);
+
+    while (stack.length) {
+      if (stack.length === N) {
+        let sum = 0;
+        for (let i = 0; i < N - 1; i++) sum += Math.abs(arr[i] - arr[i + 1]);
+        result.push(sum);
+      }
+      for (let i = 0; i < N; i++) {
+        if (visited[i]) continue;
+        visited[i] = true;
+        stack.push(arr[i]);
+      }
+    }
   }
+
   let max = result.reduce(function (a, b) {
     return Math.max(a, b);
   });
-  console.log(max);
-};
-
-const backtracking = (cnt, stack, visited, result) => {
-  if (cnt === N) {
-    let sum = 0;
-    for (let i = 0; i < N - 1; i++) sum += Math.abs(arr[i] - arr[i + 1]);
-    result.push(sum);
-    return;
-  }
-  for (let i = 0; i < N; i++) {
-    if (visited[i]) continue;
-    visited[i] = true;
-    stack.push(arr[i]);
-    backtracking(cnt + 1, stack, visited, result);
-    stack.pop();
-    visited[i] = false;
-    cnt -= 1;
-  }
+  console.log(result);
 };
 
 solution(N, arr);
