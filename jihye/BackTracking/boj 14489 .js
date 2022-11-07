039c69fb9c9d9fbb537a4d0d4e39ae5daf1d57dd
jihye_son <jiiihyeee98@gmail.com>
2022-11-07 17:25:57 +0900

    푸는중...

--- jihye/BackTracking/boj 14489 .js
+++ jihye/BackTracking/boj 14489 .js
@@ -1,15 +1,55 @@
-let input = require('fs').readFileSync(__dirname+'/input.txt').toString().split('\n');
-var n = input[0].split(' ');
+let input = require('fs').readFileSync(__dirname+'/input.txt').toString().split('\n');  
+var n = Number(input[0].split(' '));    //모인 사람의 수 
 
-var m = input[1].split(' '); //뽑은 자연수 m개 
-var a = n[0]    //a개의 카드들
-var b = parseInt(n[1])  //b에 가까운 숫자를 만듦
+var a = input[1].split(' '); //뽑은 자연수 m개 
+var b = input[2].split(' ')
+var c = input[3].split(' ')
+var d = input[3].split(' ')
 
-let answer =''
+var arr = []
+arr.push(a)
+arr.push(b)
+arr.push(c)
+arr.push(d)
+
+var answer = []
+
+function solution(n, arr){
+
+    let assi = Array.from({length:n}, ()=>0 )
+    let ch = Array.from({length:n}, ()=> 0)
+    let a = Array.from({length:n}, (v,i)=> i+1 )
+
+    function DFS(k){
+        const set = new Set( assi )
+        let an = [...set]
+        
+        if(an.length === n){ //종료조건 
+            for (let i =0 ; i < an.length; i ++){
+               // an[i], an[i+1], //an[i+2], an[i+3]
+               // an[i+1], an[i], //an[i+3], an[i+2]
+                
+
+            }
+        }
+        
+        for ( let i =0 ; i < n ; i ++){
+            if ( !ch[i]){
+                assi[k] = i
+                ch[i]=1 
+                DFS(k+1)
+                ch[i]=0
+            }
+        }
+
+    }
+
+    DFS(0)
+    return answer 
+}
 
 
-
-console.log(solution(a,b,m))
+console.log(solution(n,arr))     
 
 
 
--- jihye/Stack/boj 1406.js
+++ jihye/Stack/boj 1406.js
@@ -1,14 +1,49 @@
 let input = require('fs').readFileSync(__dirname+'/input.txt').toString().split('\n');
-var n = parseInt(input[0].split(' ')); //명령 개수 
-//input[1]~input[14]까지의 명령이 있음
+var strr = input[0].split(' '); // 단어
+var n = parseInt(input[1])  //명령어 개수 
+
+// 명령어는 input[2]~ input[n+1]이렇게 존재함
 
 
-function solution(n){
+function solution(strr,n){
+    let str = strr[0].split('')
 
+    for ( let i =2 ; i <= n+1; i ++){
+        let a = input[i].split(' ') //a[0] 은 명령어
+        let pos = str.length-1  //커서의 위치 
+        
+        switch(a[0]){
+            
+            case 'L' :
+                if(pos === 0 ) break; 
+                else pos--
+            break;
+
+            case 'D' :
+                if(pos === str.length ) break; 
+                else pos++
+            break;
+
+            case 'B' :
+                if(pos === 0 ) break; 
+                else str.pop()
+            break;
+
+            case 'P' :
+                pos++
+                str[pos] = a[1]
+            break;
+            
+        }
+
+
+
+    }
+    return str.join('')
 }
 
 
-console.log(solution(n))
+console.log(solution(strr,n))
 
 
 
