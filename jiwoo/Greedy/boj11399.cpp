08f7a28738c025dd62dfbfdd9fa8ead851f58c9e
jiou6 <jiou6@DESKTOP-GNEOFKK>
2022-11-17 15:08:20 +0900

    지우 그리디

---
+++ jiwoo/Greedy/boj11399.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    vector<int> v;
+    int N;
+    int a;
+    int tmp = 0;
+    int ans = 0;
+    cin >> N;
+    
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a;
+        v.push_back(a);
+    }
+    while (v.size())
+    {
+        tmp = tmp + *min_element(v.begin(),v.end());
+        ans = ans + tmp;
+        v.erase(min_element(v.begin(),v.end()));
+    }
+
+    cout << ans;
+    return 0;
+}
