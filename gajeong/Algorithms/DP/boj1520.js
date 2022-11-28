020f4628f68178fc869910c7ab7a6808b89343cd
gajeong <rlarkwjd127@gmail.com>
2022-11-28 17:02:33 +0900

    BOJ 1520 / 시간초과

--- gajeong/Algorithms/DP/boj1520.js
+++ gajeong/Algorithms/DP/boj1520.js
@@ -1,44 +1,50 @@
 const fs = require('fs');
 const input = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
-    : `4 5
+    process.platform === 'linux'
+        ? fs.readFileSync('/dev/stdin').toString()
+        : `4 5
 50 45 37 32 30
 35 50 40 20 25
 30 30 25 17 28
 27 24 22 15 10`
 )
-  .trim()
-  .split('\n');
+    .trim()
+    .split('\n');
 
 /*
  */
 const main = (input) => {
-  const [N, M] = input[0].split(' ').map(Number);
-  const Map = [];
-  const visited = Array.from(Array(N), () => Array(M).fill(-1));
-  for (let i = 1; i <= N; i++) {
-    Map.push(input[i].split(' ').map(Number));
-  }
-
-  //상우하좌
-  const dx = [0, 1, 0, -1];
-  const dy = [-1, 0, 1, 0];
-
-  visited[0][0] = 1;
-  const DP = (x, y, val, way) => {
-    if (x < 0 || y < 0 || x > M - 1 || y > N - 1) return;
-    if (Map[y][x] >= val && x != 0 && y != 0) return;
-    if (visited[y][x] === -1) visited[y][x] = way;
-    else visited[y][x] += way;
-    for (let d = 0; d < 4; d++) {
-      DP(x + dx[d], y + dy[d], Map[y][x], visited[y][x]);
+    const [N, M] = input[0].split(' ').map(Number);
+    const Map = [];
+    const visited = Array.from(Array(N), () => Array(M).fill(-1));
+    for (let i = 1; i <= N; i++) {
+        Map.push(input[i].split(' ').map(Number));
     }
-  };
 
-  DP(0, 0, Map[0][0], 1);
+    const stack = [[0, 0]];
 
-  console.log(visited);
+    //상 우 하 좌
+    const dx = [0, 1, 0, -1];
+    const dy = [-1, 0, 1, 0];
+    visited[0][0] = 1;
+
+    while (stack.length) {
+        let [y, x] = stack.pop();
+
+        for (let i = 0; i < 4; i++) {
+            let nX = x + dx[i];
+            let nY = y + dy[i];
+            if (nX < 0 || nY < 0 || nX > M - 1 || nY > N - 1) continue;
+            // 조건에 맞는 좌표를 stack에 집어넣음
+            if (Map[nY][nX] < Map[y][x]) {
+                stack.push([nY, nX]);
+                if (visited[nY][nX] === -1) visited[nY][nX] = visited[y][x];
+                // +=1  이 맞는지도 정확히 모르겠음
+                else visited[nY][nX] += 1;
+            }
+        }
+    }
+    console.log(visited[N - 1][M - 1]);
 };
 
 main(input);
