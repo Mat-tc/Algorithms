bd8e4b138e49617c4b0972687693bac01dc17a20
gajeong <rlarkwjd127@gmail.com>
2022-11-29 14:44:49 +0900

    8, 18 실패 .. 이유가 , , ?

---
+++ concept/algorithms/TwoPointer/TwoPointer.md
@@ -0,0 +1,8 @@
+# TwoPointer
+
+<blockquote>
+배열에서 원래 이중 for 문으로 O(n^2)에 처리되는 작업을 
+2개 포인터의 움직임으로 O(N)에 해결하는 알고리즘
+
+</blockquote>
+시간복잡도 => O(N)
--- gajeong/Programmers/양궁대회.js
+++ gajeong/Programmers/양궁대회.js
@@ -2,12 +2,31 @@ function solution(n, info) {
     var answer = [];
     const list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
     let result = [];
-    const res = pwc([], 0, 3, list, result);
-    console.log(res);
+    let max = 1;
+    result = pwc([], 0, n, list, result);
 
-    return answer;
+    for (let i = 0; i < result.length; i++) {
+        let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        for (let j = 0; j < n; j++) {
+            arr[10 - result[i][j]] += 1;
+        }
+        let k = 0;
+        let apeach = 0;
+        let rion = 0;
+        for (k; k < 11; k++) {
+            if (arr[k] > info[k]) rion += 10 - k;
+            if (arr[k] <= info[k] && info[k] > 0) apeach += 10 - k;
+        }
+        if (k == 11 && rion - apeach >= max) {
+            answer = arr;
+            max = rion - apeach;
+        } else continue;
+    }
+
+    return answer.length != 0 ? answer : [-1];
 }
 
+//중복조합
 function pwc(items, idx, k, list, result) {
     if (items.length === k) {
         result.push(items);
@@ -20,4 +39,4 @@ function pwc(items, idx, k, list, result) {
     return result;
 }
 
-solution(5, [2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]);
+solution(9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1]);
