fe11ff183e626a410a3089787e28bb09f932960a
gajeong <rlarkwjd127@gmail.com>
2022-12-05 15:13:15 +0900

    수정중

--- gajeong/DataStructure/Hash/boj20166.js
+++ gajeong/DataStructure/Hash/boj20166.js
@@ -13,35 +13,33 @@ cab`
     .trim()
     .split('\n');
 
-const [M, N, K] = input[0].split(' ').map(Number);
-const MAP = [];
-const str = [];
-for (let i = 1; i <= M; i++) {
-    MAP.push(input[i].split(''));
-}
+const main = (input) => {
+    const [M, N, K] = input.shift().split(' ').map(Number);
+    const map = {};
+    const answer = [];
 
-for (let i = M + 1; i <= M + K; i++) {
-    str.push(input[i].split(''));
-}
-
-console.log(str);
-const main = () => {
-    //북쪽부터 시계방향
     const dx = [0, 1, 1, 1, 0, -1, -1, -1];
     const dy = [-1, -1, 0, 1, 1, 1, 0, -1];
 
-    let x = 0;
-    let y = 0;
-};
-
-const DFS = (x, y) => {
-    while (true) {
-        if (x == N - 1 && y == M - 1) break;
-        if (x < 0) x = M;
-        if (x > N - 1) x = 0;
-        if (y < 0) y = N;
-        if (y > M - 1) y = 0;
+    //문자마다 좌표 위치 배열 객체 만듬
+    for (let j = 0; j < M; j++) {
+        for (let i = 0; i < N; i++) {
+            if (map[input[j][i]]) map[input[j][i]].push([i, j]);
+            else map[input[j][i]] = [[i, j]];
+        }
     }
+
+    //경우의 수 count
+    for (let c = M + 1; c <= M + K + 1; c++) {
+        let cnt = 0;
+        for (let y = 0; y < M; y++) {
+            for (let x = 0; x < N; x++) {
+                check(x);
+            }
+        }
+    }
+
+    const check = () => {};
 };
 
 main(input);
