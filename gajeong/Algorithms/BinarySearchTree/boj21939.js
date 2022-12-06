f058621a147c4d3fb043023f459f08a094a02c94
gajeong <rlarkwjd127@gmail.com>
2022-12-06 18:30:45 +0900

    수정중

--- gajeong/Algorithms/BinarySearchTree/boj21939.js
+++ gajeong/Algorithms/BinarySearchTree/boj21939.js
@@ -2,13 +2,20 @@ const fs = require('fs');
 const input = (
     process.platform === 'linux'
         ? fs.readFileSync('/dev/stdin').toString()
-        : `2
+        : `5
 1000 1
-2000 1
-4
-recommend -1
+1001 2
+19998 78
+2667 37
+2042 55
+8
+add 1402 59
+recommend 1
 solved 1000
-add 1000 1000
+solved 19998
+recommend 1
+recommend -1
+solved 1001
 recommend -1`
 )
     .trim()
@@ -16,77 +23,74 @@ recommend -1`
 
 const main = (input) => {
     // key : 문제번호, value : 문제 난이도
-    const q = {};
-    const l = {};
+    const Q = {};
+    const LV = {};
+
+    const insert = (num, level) => {
+        if (Q[num]) {
+            let before_level = Q[num];
+            let before_arr = LV[before_level];
+            //이전 레벨에서 해당 문제 번호 삭제
+            let res = before_arr.filter((e) => e != num);
+            if (res.length === 0) {
+                delete LV.before_level;
+            } else LV[before_level] = res;
+
+            //새 레벨에 해당 문제 삽입
+            if (LV[level]) LV[level].push(num);
+            else LV[level] = [num];
+        } else {
+            if (LV[level]) LV[level].push(num);
+            else LV[level] = [num];
+        }
+        Q[num] = level;
+    };
+
+    const del = (num) => {
+        let lv = Q[num];
+        let p = LV[lv];
+        let res = p.filter((e) => e != num);
+        if (res.length === 0) delete LV.lv;
+        else LV[lv] = res;
+        delete Q.num;
+    };
 
     const N = Number(input[0]);
     //입력처리
     for (let i = 1; i <= N; i++) {
-        let [num, level] = input[i].split(' ').map(Number);
-        //문제번호가 이미 있으면,
-        if (q[num]) {
-            let before_level = q[num];
-            let arr = l[before_level];
-            let res = arr.filter((e) => e != num);
-            l[before_level] = res;
-
-            //새로운 번호로 갱신
-            q[num] = level;
-            if (l[level]) {
-                l[level].push(num);
-            } else {
-                l[level] = [num];
-            }
-        } else {
-            q[num] = level;
-            if (l[level]) {
-                l[level].push(num);
-            } else {
-                l[level] = [num];
-            }
-        }
+        let [num, level] = input[i].split(' ');
+        insert(num, level);
     }
+
     let answer = [];
+    //명령어 처리
     const M = Number(input[N + 1]);
     for (let i = 2 + N; i <= 1 + N + M; i++) {
         let od = input[i].split(' ');
-        switch (od[0]) {
-            case 'add':
-                if (q[od[1]]) {
-                    let arr = l[q[od[1]]];
-                    let res = arr.filter((e) => e != od[1]);
-                    l[q[od[1]]] = res;
-                    q[od[1]] = Number(od[2]);
+        let order = od[0];
+        switch (order) {
+            case 'recommend':
+                let way = od[1];
+                if (way === '1') {
+                    let high_level = Math.max(...Object.keys(LV));
+                    answer.push(Math.max(...LV[high_level]));
                 } else {
-                    q[od[1]] = Number(od[2]);
-                    l[Number(od[2])] = od[1];
+                    let low_level = Math.min(...Object.keys(LV));
+                    answer.push(Math.min(...LV[low_level]));
                 }
                 break;
-            case 'recommend':
-                if (od[1] === '1') {
-                    let key = Math.max(...Object.keys(l));
-
-                    let arr = l[key];
-                    answer.push(Math.max(arr));
-                } else {
-                    let key = Math.min(...Object.keys(l));
-                    let arr = l[key];
-                    answer.push(Math.min(...arr));
-                }
+            case 'add':
+                let num = od[1];
+                let level = od[2];
+                insert(num, level);
                 break;
             case 'solved':
-                let c = od[1];
-                let lv = q[c];
-                let arr = l[lv];
-                let res = arr.filter((e) => e != c);
-
-                if (res.length === 0) delete l[lv];
-                else l[lv] = res;
-                delete q[c];
+                del(od[1]);
+                break;
         }
     }
-    console.log(answer);
-    // console.log(answer.join('\n'));
+
+    console.log(answer.join('\n'));
 };
 
 main(input);
