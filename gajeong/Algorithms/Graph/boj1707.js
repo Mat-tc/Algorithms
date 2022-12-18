b02e3aa4744a31d3ab9f81354d7d3dedb676d40d
gajeong <rlarwkjd127@gmail.com>
2022-12-18 20:07:35 +0900

    BOJ 1707 / 수정중

--- gajeong/Algorithms/Graph/boj1707.js
+++ gajeong/Algorithms/Graph/boj1707.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = (
     process.platform === "linux"
         ? fs.readFileSync("/dev/stdin").toString()
-        : `2
+        : `3
 3 2
 1 3
 2 3
@@ -11,6 +11,9 @@ const input = (
 2 3
 3 4
 4 2
+3 2 
+1 3
+2 3
 `
 )
     .trim()
@@ -26,22 +29,27 @@ for (let i = 0; i < T; i++) {
     let [V, E] = input[idx].split(" ").map(Number);
     let visited = Array(V + 1).fill(-1);
     let node = Array.from({ length: V + 1 }, () => []);
+
+    let q = [];
     for (let k = 1 + idx; k <= E + idx; k++) {
         let [start, end] = input[k].split(" ").map(Number);
         node[start].push(end);
         node[end].push(start);
+        if (k == 1 + idx) {
+            q.push(start);
+            visited[start] = 0;
+        }
     }
+
     let point = 0;
-    let st = 0;
-    let [start_num, end] = input[idx + 1].split(" ").map(Number);
     let status = true;
-    let q = [start_num];
-    visited[start_num] = 0;
+
     while (point < q.length) {
         let arr = node[q[point]];
+        let st = visited[q[point]];
         for (let k = 0; k < arr.length; k++) {
             if (visited[arr[k]] == -1) {
-                visited[arr[k]] = st == 0 ? 1 : 0;
+                st == 0 ? (visited[arr[k]] = 1) : (visited[arr[k]] = 0);
                 q.push(arr[k]);
             } else {
                 if (visited[arr[k]] == st) {
@@ -52,7 +60,9 @@ for (let i = 0; i < T; i++) {
         }
 
         point++;
-        st == 0 ? (st = 1) : (st = 0);
+    }
+    for (let k = 1; k <= visited.length; k++) {
+        if (visited[k] == -1) status = false;
     }
     status == true ? answer.push("YES") : answer.push("NO");
 
