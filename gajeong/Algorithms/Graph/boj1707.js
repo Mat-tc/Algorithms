ae87c6542a43afca5efcae7163c34e968d0be25b
gajeong <rlarwkjd127@gmail.com>
2022-12-19 23:19:43 +0900

    BOJ 1707 /수정중

--- gajeong/Algorithms/Graph/boj1707.js
+++ gajeong/Algorithms/Graph/boj1707.js
@@ -2,18 +2,10 @@ const fs = require("fs");
 const input = (
     process.platform === "linux"
         ? fs.readFileSync("/dev/stdin").toString()
-        : `3
+        : `1
 3 2
-1 3
-2 3
-4 4
 1 2
-2 3
-3 4
-4 2
-3 2 
 1 3
-2 3
 `
 )
     .trim()
@@ -25,32 +17,30 @@ const input = (
 const T = Number(input.shift());
 const answer = [];
 let idx = 0;
+let node = [];
+visited = [];
+let status = true;
 for (let i = 0; i < T; i++) {
     let [V, E] = input[idx].split(" ").map(Number);
-    let visited = Array(V + 1).fill(-1);
-    let node = Array.from({ length: V + 1 }, () => []);
+    visited = Array(V + 1).fill(-1);
+    node = Array.from({ length: V + 1 }, () => []);
 
-    let q = [];
     for (let k = 1 + idx; k <= E + idx; k++) {
         let [start, end] = input[k].split(" ").map(Number);
         node[start].push(end);
         node[end].push(start);
-        if (k == 1 + idx) {
-            q.push(start);
-            visited[start] = 0;
-        }
     }
 
-    let point = 0;
-    let status = true;
+    visited[1] = 1;
+    status = true;
 
-    while (point < q.length) {
-        let arr = node[q[point]];
-        let st = visited[q[point]];
+    for (let j = 1; j <= V; j++) {
+        let arr = node[j];
+        let st = visited[j];
+        if (st == -1) visited[j] = 1;
         for (let k = 0; k < arr.length; k++) {
             if (visited[arr[k]] == -1) {
                 st == 0 ? (visited[arr[k]] = 1) : (visited[arr[k]] = 0);
-                q.push(arr[k]);
             } else {
                 if (visited[arr[k]] == st) {
                     status = false;
@@ -58,12 +48,8 @@ for (let i = 0; i < T; i++) {
                 }
             }
         }
+    }
 
-        point++;
-    }
-    for (let k = 1; k <= visited.length; k++) {
-        if (visited[k] == -1) status = false;
-    }
     status == true ? answer.push("YES") : answer.push("NO");
 
     idx += E + 1;
