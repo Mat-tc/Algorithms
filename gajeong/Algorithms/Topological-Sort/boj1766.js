2846e13448d3d087bfddccba2b834bc060127702
gajeong <rlarkwjd127@gmail.com>
2022-12-26 18:30:30 +0900

    수정중

--- gajeong/Algorithms/Topological-Sort/boj1766.js
+++ gajeong/Algorithms/Topological-Sort/boj1766.js
@@ -12,10 +12,11 @@ solution(input);
 function solution(input) {
     const [N, M] = input[0].split(' ').map(Number);
     const in_degree = Array(N + 1).fill(0);
-    const visited = Array(N + 1).fill(false);
     const node = [];
     for (let i = 1; i <= M; i++) {
         let [pre, after] = input[i].split(' ').map(Number);
+        if (!node[pre]) node[pre] = [];
+        node[pre].push(after);
         in_degree[after] += 1;
     }
 
@@ -23,11 +24,18 @@ function solution(input) {
     for (let i = 1; i <= N; i++) {
         if (in_degree[i] == 0) {
             stack.push(i);
-            visited[i] = true;
         }
     }
     const answer = [];
     while (stack.length) {
         let num = stack.pop();
+        let arr = node[num];
+        for (let i = 0; i < arr.length; i++) {
+            in_degree[arr[i]] -= 1;
+            if (in_degree[arr[i]] == 0) {
+                stack.push(arr[i]);
+            }
+        }
     }
+    console.log(answer);
 }
