af7dce35e89d5d6874496dabe274351f1a1b3b3d
gajeong <rlarkwjd127@gmail.com>
2023-01-02 11:52:15 +0900

    서강그라운드

--- gajeong/Algorithms/Floyd/boj14938.js
+++ gajeong/Algorithms/Floyd/boj14938.js
@@ -1,12 +1,51 @@
 //예시 입력 시,
-const fs = require("fs");
+const fs = require('fs');
+
 let input =
-  process.platform === "linux"
-    ? fs.readFileSync("/dev/stdin").toString()
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
     : fs
-        .readFileSync("C:/project/Algorithms/gajeong/예제.txt")
+        .readFileSync('C:/project/Algorithms/gajeong/예제.txt')
         .toString()
         .trim()
-        .split("\r\n");
+        .split('\r\n');
 
-function solution() {}
+solution(input);
+
+function solution(input) {
+  const [n, m, r] = input[0].split(' ').map(Number);
+  const items = [0, ...input[1].split(' ').map(Number)];
+  const map = Array.from(Array(n + 1), () => Array(n + 1).fill(m + 1));
+  const answer = Array(n + 1).fill(0);
+  for (let i = 1; i <= n; i++) {
+    map[i][i] = 0;
+  }
+
+  //비용 삽입
+  for (let i = 2; i < 2 + r; i++) {
+    let [start, end, cost] = input[i].split(' ').map(Number);
+    map[start][end] = cost;
+    map[end][start] = cost;
+  }
+
+  //플로이드
+  for (let middle = 1; middle <= n; middle++) {
+    for (let start = 1; start <= n; start++) {
+      for (let end = 1; end <= n; end++) {
+        if (middle == start || start == end || end == middle) continue;
+        map[start][end] = Math.min(
+          map[start][end],
+          map[start][middle] + map[m입ddle][end]
+        );
+      }
+    }
+  }
+
+  //정답 출력
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (map[i][j] <= m) answer[i] += items[j];
+    }
+  }
+  console.log(Math.max(...answer));
+}
--- input.js
+++ input.js
@@ -2,9 +2,9 @@ const rl = require('readline').createInterface({ input: process.stdin });
 const arr = [];
 
 rl.on('line', (l) => {
-    arr.push(l);
+  arr.push(l);
 }).on('close', () => {
-    solution(arr);
+  solution(arr);
 });
 
 function solution(input) {}
@@ -12,10 +12,10 @@ function solution(input) {}
 //예시 입력 시,
 const fs = require('fs');
 let input =
-    process.platform === 'linux'
-        ? fs.readFileSync('/dev/stdin').toString()
-        : fs
-              .readFileSync('C:/project/Algorithms/gajeong/예제.txt')
-              .toString()
-              .trim()
-              .split('\r\n');
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : fs
+        .readFileSync('C:/project/Algorithms/gajeong/예제.txt')
+        .toString()
+        .trim()
+        .split('\r\n');
