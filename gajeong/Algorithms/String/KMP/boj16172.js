d0eb935adfef60ebf7f8c4e7320824fa51b0003b
gajeong <rlarkwjd127@gmail.com>
2023-01-13 00:47:05 +0900

    수정숭

--- gajeong/Algorithms/String/KMP/boj16172.js
+++ gajeong/Algorithms/String/KMP/boj16172.js
@@ -1,21 +1,19 @@
-const fs = require('fs');
+const fs = require("fs");
 let input = fs
-  .readFileSync('C:/project/Algorithms/gajeong/예제.txt')
+  .readFileSync("C:/project/Algorithms/gajeong/예제.txt")
   .toString()
   .trim()
-  .split('\r\n');
+  .split("\r\n");
 solution(input);
 
 function solution(input) {
-  const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
   let str = input[0]
-    .split('')
+    .split("")
     .filter((el) => !nums.includes(el))
-    .join('');
+    .join("");
 
   let sub = input[1];
-  let max_idx = sub.length - 1;
-  let list = [];
 
   console.log(findString(str, sub) ? 1 : 0);
 
@@ -30,19 +28,19 @@ function solution(input) {
     for (let i = 0; i <= parentSize - patternSize; i++) {
       if (i == 0) {
         for (let j = 0; j < patternSize; j++) {
-          parentHash += parent.codePointAt(j) * 2 ** (patternSize - j);
-          patternHash += pattern.codePointAt(j) * 2 ** (patternSize - j);
+          parentHash += parent.codePointAt(j) * 7 ** (patternSize - j);
+          patternHash += pattern.codePointAt(j) * 7 ** (patternSize - j);
         }
       }
-      if (parentHash == patternHash) {
+      if (parentHash % 4567 == patternHash % 4567) {
         for (let j = i; j < i + patternSize; j++) {
           if (parent[j] != pattern[j - i]) break;
           if (j == i + patternSize - 1) return true;
         }
       }
-      parentHash -= parent.codePointAt(i) * 2 ** patternSize;
-      parentHash *= 2;
-      parentHash += parent.codePointAt(i + patternSize) * 2;
+      parentHash -= parent.codePointAt(i) * 7 ** patternSize;
+      parentHash *= 7;
+      parentHash += parent.codePointAt(i + patternSize) * 7;
     }
     return false;
   }
--- gajeong/Programmers/LV2/택배_배달과_수거하기.js
+++ gajeong/Programmers/LV2/택배_배달과_수거하기.js
@@ -1,26 +1,28 @@
 function solution(cap, n, deliveries, pickups) {
   var answer = 0;
+  let d = cap;
   let p = 0;
-  let d = 0;
-  let distance = 0;
-  for (let i = n - 1; i > -1; i--) {
+  let length = 0;
+  let idx = n - 1;
+  for (let i = n - 1; i >= 0; i--) {
     if (deliveries[i] || pickups[i]) {
-      d += deliveries[i];
-      p += pickups[i];
-      distance++;
-    }
-
-    if (d > cap || pickups > cap) {
-      answer += (i + distance + 1) * 2;
-      d - cap < 0 ? (d = 0) : (d -= cap);
-      p - cap < 0 ? (p = 0) : (p -= cap);
-      distance = 0;
+      idx = i;
+      break;
     }
   }
-  answer += distance * 2;
-  console.log(answer);
+  for (let i = idx; i >= 0; i--) {
+    d -= deliveries[i];
+    p += pickups[i];
+    length++;
+    if (d < 0 || p > cap) {
+      answer += (length + i) * 2;
+      d + cap > cap ? (d = cap) : (d = d + cap);
+      p <= cap ? (p = 0) : (p = pickups[i]);
+      length = 1;
+    }
+  }
+  answer += length * 2;
   return answer;
 }
-
 solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]);
 solution(2, 7, [1, 0, 2, 0, 1, 0, 2], [0, 2, 0, 1, 0, 2, 0]);
