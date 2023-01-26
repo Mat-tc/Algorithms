ad6fbbd0067e9edf041d27ca4cc1bbef202fb60d
gajeong <rlarkwjd127@gmail.com>
2023-01-26 11:46:57 +0900

    우박수열정적분

--- gajeong/Programmers/LV2/우박수열_정적분.js
+++ gajeong/Programmers/LV2/우박수열_정적분.js
@@ -1,4 +1,35 @@
 function solution(k, ranges) {
+  var width = [];
   var answer = [];
+  while (k !== 1) {
+    let before = k;
+    if (k % 2 == 0) {
+      k /= 2;
+    } else {
+      k = k * 3 + 1;
+    }
+    let after = k;
+    width.push((before + after) / 2);
+  }
+  ranges.forEach((range) => {
+    let [start, end] = range;
+    let sum = 0;
+    let status = false;
+    for (let i = start; i < width.length + end; i++) {
+      sum += width[i];
+      status = true;
+    }
+    if (start > width.length + end) answer.push(-1);
+    else if (start == width.length + end) answer.push(0);
+    else answer.push(sum);
+  });
+  console.log(answer);
   return answer;
 }
+
+solution(5, [
+  [0, 0],
+  [0, -1],
+  [2, -3],
+  [3, -3],
+]);
