19863ae0ea9e7b890dd6847560b723b917ee97fd
gajeong <rlarkwjd127@gmail.com>
2023-02-14 00:22:36 +0900

    인사고과 수정하기

--- gajeong/Programmers/LV3/인사고과.js
+++ gajeong/Programmers/LV3/인사고과.js
@@ -1,39 +1,39 @@
 function solution(scores) {
-  let max_1 = -1;
-  let max_2 = -1;
-  for (let i = 0; i < scores.length; i++) {
-    if (max_1 < scores[i][0]) {
-      max_1 = scores[i][0];
-    }
-    if (max_2 < scores[i][1]) {
-      max_2 = scores[i][1];
-    }
-  }
-
   let mine = scores.shift();
   let sum = mine[0] + mine[1];
   scores.sort((a, b) => {
-    return b[0] + b[1] - (a[0] + a[1]);
+    if (b[0] + b[1] > a[0] + a[1]) return b[0] + b[1] - (a[0] + a[1]);
+    else if (b[0] + b[1] == a[0] + a[1] && b[0] != a[0]) return b[0] - a[0];
+    else return a[0] - b[0];
   });
 
   let rank = 1;
 
-  if (mine[0] < max_1 && mine[1] < max_2) return -1;
+  console.log(scores);
   for (let i = 0; i < scores.length; i++) {
+    if (mine[0] < scores[i][0] && mine[1] < scores[i][1]) return -1;
     // 내 점수의 합이 정렬된 점수들의 합보다 같거나 클때, 랭킹 리턴
     if (sum >= scores[i][0] + scores[i][1]) return rank;
     // 해당 수 둘다 의 값이 max 보다 작을때 랭크 안올림
-    if (scores[i][0] < max_1 && scores[i][1] < max_2) continue;
+    if (i == 0) {
+      rank++;
+      continue;
+    }
+    if (scores[i][0] < scores[0][0] && scores[i][1] < scores[0][1]) continue;
     rank++;
   }
 }
 
 console.log(
   solution([
-    [7, 1],
-    [10, 1],
+    [3, 4],
+    [10, 3],
     [9, 0],
     [6, 2],
+    [7, 1],
+    [5, 3],
+    [4, 4],
+    [3, 5],
     [6, 1],
   ])
 );
