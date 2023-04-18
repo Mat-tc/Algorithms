726fbc5c82e17c5609549ffb94b433f3362d5be1
gajeong <rlarkwjd127@gmail.com>
2023-04-18 10:51:54 +0900

    당구연습

--- gajeong/Programmers/LV2/당구연습.js
+++ gajeong/Programmers/LV2/당구연습.js
@@ -2,19 +2,25 @@ function solution(m, n, startX, startY, balls) {
   var answer = [];
 
   const dir = [
-    [-startX, startY],
-    [2 * m - startX, startY],
-    [startX, 2 * n - startY],
-    [startX, -startY],
+    [-startX, startY], //왼쪽벽 친 경우
+    [2 * m - startX, startY], // 오른쪽 벽 친 경우
+    [startX, 2 * n - startY], // 위쪽 벽 친 경우
+    [startX, -startY], //아래 벽 친 경우
   ];
 
   balls.forEach((el) => {
     let [x, y] = [el[0], el[1]];
     let dmp = [];
-    for (let d = 0; d < 4; d++) {
-      if (dir[d][0] === x || dir[d][1] === y) continue;
-      dmp.push((dir[d][0] - x) ** 2 + (dir[d][1] - y) ** 2);
-    }
+
+    if (!(startX > x && startY == y))
+      dmp.push((dir[0][0] - x) ** 2 + (dir[0][1] - y) ** 2);
+    if (!(startX < x && startY == y))
+      dmp.push((dir[1][0] - x) ** 2 + (dir[1][1] - y) ** 2);
+    if (!(startY < y && startX == x))
+      dmp.push((dir[2][0] - x) ** 2 + (dir[2][1] - y) ** 2);
+    if (!(startY > y && startX == x))
+      dmp.push((dir[3][0] - x) ** 2 + (dir[3][1] - y) ** 2);
+
     answer.push(Math.min(...dmp));
   });
 
