fa5aead7194528674ed762201dab297b2c572189
gajeong <rlarkwjd127@gmail.com>
2023-04-18 11:42:37 +0900

    광물캐기 - 재귀로 다시 수정해야함

--- gajeong/Programmers/LV2/광물캐기_2.js
+++ gajeong/Programmers/LV2/광물캐기_2.js
@@ -1,52 +1,30 @@
 function solution(picks, minerals) {
-  const nums = [];
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < picks[i]; j++) {
-      nums.push(i + 1);
+  const combination = (idx, now, tired) => {
+    if (idx >= minerals.length || picks.every((el) => el === 0)) return tired;
+
+    for (let i = 0; i < 5; i++) {
+      ++idx;
+      if (idx >= minerals.length) break;
+
+      if (now == 0) {
+        tired += 1;
+      } else if (now == 1) {
+        if (minerals[idx] === 'diamond') tired += 5;
+        else tired += 1;
+      } else {
+        if (minerals[idx] === 'diamond') tired += 25;
+        else if (minerals[idx] === 'iron') tired += 5;
+        else tired += 1;
+      }
     }
-  }
-  const selectNum = Math.min(parseInt(minerals.length / 5) + 1, nums.length);
 
-  const C = combination(nums, selectNum);
-  const answer = [];
-  C.forEach((order) => {
-    let idx = -1;
-    let sum = 0;
-    let l = Math.min(5 * selectNum, minerals.length);
-    for (let i = 0; i < l; i++) {
-      if (i % 5 == 0) ++idx;
-      sum += getTired(order[idx], minerals[i]);
+    for (k = 0; k < 3; k++) {
+      if (picks[k] != 0) {
+        picks[k] -= 1;
+        combination(idx, k, 0);
+      }
     }
-    answer.push(sum);
-  });
-
-  return Math.min(...answer);
-}
-
-function combination(arr, selectNum) {
-  const res = [];
-  if (selectNum === 1) return arr.map((value) => [value]);
-  arr.forEach((item, idx, origin) => {
-    const remain = origin.slice(idx + 1);
-    const reCall = combination(remain, selectNum - 1);
-    const attach = reCall.map((value) => [item, ...value]);
-
-    res.push(...attach);
-  });
-  return res;
-}
-
-function getTired(g, name) {
-  if (g == 1) return 1;
-  if (g == 2) {
-    if (name == 'diamond') return 5;
-    else return 1;
-  }
-  if (g == 3) {
-    if (name == 'diamond') return 25;
-    else if (name == 'iron') return 5;
-    else return 1;
-  }
+  };
 }
 
 console.log(
